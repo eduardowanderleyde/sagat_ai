41aab7fb94fb235b1435cf9114ab056e216a182a
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2025-05-29 01:21:55 -0300

    chore: add database and Sidekiq configuration

---
+++ db/migrate/20250529023524_change_agency_on_bank_accounts.rb
@@ -0,0 +1,8 @@
+class ChangeAgencyOnBankAccounts < ActiveRecord::Migration[8.0]
+  def change
+   
+    change_column_default :bank_accounts, :agency, from: '0001', to: nil
+    
+    change_column_null    :bank_accounts, :agency, false
+  end
+end
