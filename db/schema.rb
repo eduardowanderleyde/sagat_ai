ac1e14b7e8f74a68a3caa08e2da34e4839ec33d2
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2025-05-29 21:26:21 -0300

    chore: protect users#index for admin only and remove pending test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_29_023524) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_30_001805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_29_023524) do
     t.string "cpf"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
   add_foreign_key "bank_accounts", "users"
